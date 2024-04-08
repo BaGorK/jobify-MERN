3736e056a50e8fb3f181196a41dcf0d2b2fd2db1
BaGorK <edmealemkassahun@gmail.com>
2024-04-09 01:53:31 +0300

    feat: register page setup

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,38 @@
 import { Link } from 'react-router-dom';
 
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Logo } from '../components';
+
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to='/login'>Login</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+          <input
+            type='text'
+            id='name'
+            name='name'
+            className='form-input'
+            defaultValue='Edmealem'
+            required
+          />
+        </div>
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 }
 
