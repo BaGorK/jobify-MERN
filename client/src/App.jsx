a53f5a64291659fdd3f4e6b9b1ebf54845d1751b
BaGorK <edmealemkassahun@gmail.com>
2024-04-08 14:09:20 +0300

    feat: react-router initial setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>home page</h1>,
+  },
+  {
+    path: '/about',
+    element: <h1>about page</h1>,
+  },
+]);
+
 function App() {
-  return <div>Jobify</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
