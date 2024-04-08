78bc4126c49b2d5a9c09e5559d15a2d254864dc2
BaGorK <edmealemkassahun@gmail.com>
2024-04-09 01:00:42 +0300

    feat: styled components | wrapper

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: red;
+  h1 {
+    color: white;
+  }
+  .content {
+    background-color: blue;
+    color: yellow;
+  }
+`;
+
 function Landing() {
-  return <h1>Landing page</h1>;
+  return (
+    <Wrapper>
+      <h1>Landing page</h1>
+      <div className='content'>some content</div>
+    </Wrapper>
+  );
 }
 
 export default Landing;
