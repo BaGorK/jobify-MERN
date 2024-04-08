84d6b9d5ecbc98307213697c220948299f7346d4
BaGorK <edmealemkassahun@gmail.com>
2024-04-09 01:39:14 +0300

    feat: build an error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,15 +1,29 @@
 import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
 function Error() {
   const error = useRouteError();
 
-  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt='not found' />
+          <h3>Ohh! page not found</h3>
+          <p>We can&rsquo;t seem to find the page you are looking for</p>
+          <Link to='/dashboard'>Back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
 
   return (
-    <div>
-      <h1>Error page</h1>
-      <Link to='/'>back</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 }
 
