fb5f504edd5aa757f4fe0a375157db6ad4d83d45
BaGorK <edmealemkassahun@gmail.com>
2024-04-08 15:00:07 +0300

    feat: implement nested routes with react-router-dom

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
 function HomeLayout() {
-  return <div>HomeLayout</div>;
+  return (
+    <div>
+      <nav>navbar</nav>
+      <Outlet />
+    </div>
+  );
 }
 
 export default HomeLayout;
