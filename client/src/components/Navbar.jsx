c2fa77b6ebae4d18a3e507dae89c5e9dca46a6a7
BaGorK <edmealemkassahun@gmail.com>
2024-04-11 18:58:36 +0300

    feat: setting up the general structure of navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
-import { FaHome } from 'react-icons/fa';
-
+import { FaAlignLeft } from 'react-icons/fa';
+import Logo from './Logo';
 import Wrapper from '../assets/wrappers/Navbar';
 
 function Navbar() {
   return (
     <Wrapper>
-      <h2>home</h2>
-      <FaHome />
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn'>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className='logo-text'>dashboard</h4>
+        </div>
+        <div className='btn-container'>toggle/logout</div>
+      </div>
     </Wrapper>
   );
 }
