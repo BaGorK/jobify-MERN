4b1ed7302aa514a37159e3c0c00093aaaf815493
BaGorK <edmealemkassahun@gmail.com>
2024-04-11 18:35:31 +0300

    feat: create a state for sidebar and darktheme on the dashboard layout

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,7 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
+import { useState } from 'react';
+
 function DashboardLayout() {
+  const user = { name: 'Edmealem' };
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+  const toggleDarkTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+  const logoutUser = () => {
+    console.log('User logged out');
+  };
+
   return (
     <Wrapper>
       <main className='dashboard'>
