ec84356cdd8f2fbbd022c90d87d270d336b4f5b0
BaGorK <edmealemkassahun@gmail.com>
2024-04-11 19:01:38 +0300

    feat: use the the dashboard context on the navbar to toggle the sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { FaAlignLeft } from 'react-icons/fa';
 import Logo from './Logo';
 import Wrapper from '../assets/wrappers/Navbar';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 function Navbar() {
+  const { toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
       <div className='nav-center'>
-        <button type='button' className='toggle-btn'>
+        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
           <FaAlignLeft />
         </button>
         <div>
