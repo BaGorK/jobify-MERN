3cb5da4f99cc9d31f252ab96f0fa7b5d53aea485
BaGorK <edmealemkassahun@gmail.com>
2024-04-12 15:17:51 +0300

    feat: adding a small side bar

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,10 +1,39 @@
+import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { useDashboardContext } from '../pages/DashboardLayout';
+import Logo from './Logo';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
 
 function SmallSidebar() {
   const data = useDashboardContext();
   console.log(data);
-  return <Wrapper>SmallSidebar</Wrapper>;
+  return (
+    <Wrapper>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button type='button' className='close-btn'>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map((link) => {
+              const { path, text, icon } = link;
+
+              return (
+                <NavLink to={path} key={text} className='nav-link'>
+                  <span className='icon'>{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default SmallSidebar;
