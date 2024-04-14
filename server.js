691caf9534eccfb587a4424cf09778bb5bc48fb1
BaGorK <edmealemkassahun@gmail.com>
2024-04-14 19:49:45 +0300

    feat: using json middleware and handling basic post req

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('hello from the server side...');
+});
+
+app.post('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: req.body,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
