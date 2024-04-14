7eb6beb74688a5b52113a89c368863bd3645e039
BaGorK <edmealemkassahun@gmail.com>
2024-04-14 19:39:51 +0300

    feat: setting up  a basic express app

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Sever is Working');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello from the server side...');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server is running on port 3000... ');
+});
