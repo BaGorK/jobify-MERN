7bea1dbfed73028df305559182b393ddf4fd126f
BaGorK <edmealemkassahun@gmail.com>
2024-04-14 20:07:51 +0300

    feat: setting up morgan and env variables

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.get('/', (req, res) => {
-  res.send('hello from the server side...');
+  return res.status(200).send('hello from the server side...');
 });
 
 app.post('/', (req, res) => {
-  res.status(200).json({
+  return res.status(200).json({
     status: 'success',
     data: req.body,
   });
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log('Server is running on port 3000... ');
+  console.log(`Server is running on port ${PORT}...`);
 });
