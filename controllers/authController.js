0e13e013fe7477b42df873182fd189076ba21d8a
BaGorK <edmealemkassahun@gmail.com>
2024-04-17 22:37:04 +0300

    feat: set the first user as admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,10 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 
 export const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+
+  const user = await User.create({ ...req.body, role });
 
   res.status(StatusCodes.CREATED).json({ user });
 };
