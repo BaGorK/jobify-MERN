b970f16460a095907cb97cb707bd4275d2824316
BaGorK <edmealemkassahun@gmail.com>
2024-04-18 14:41:37 +0300

    refactor: create a  separate util function for hashPassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,12 @@
 import { StatusCodes } from 'http-status-codes';
-import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create({ ...req.body, role });
