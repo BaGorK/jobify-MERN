dcb8524cfce87347cf1c7ffa45ac19f9a8c6d638
BaGorK <edmealemkassahun@gmail.com>
2024-04-18 14:55:18 +0300

    feat: login a user | check user exit with email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import { hashPassword } from '../utils/passwordUtils.js';
+import { UnauthenticatedError } from '../errors/customErrors.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -18,5 +19,7 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) throw new UnauthenticatedError('Invalid credentials');
   res.send('login');
 };
