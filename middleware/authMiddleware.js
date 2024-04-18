32f73696302296b6515371ff331e211c5eb55f28
BaGorK <edmealemkassahun@gmail.com>
2024-04-18 15:52:06 +0300

    feat: create an Auth Middleware to protect the jobs route

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,5 @@
+export const authenticateUser = async (req, res, next) => {
+  console.log('auth middleware');
+
+  next();
+};
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 import jobRouter from './routes/jobRoute.js';
 import authRouter from './routes/authRoute.js';
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -17,7 +18,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 //  JOBS Router
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.use('*', (req, res) => {
