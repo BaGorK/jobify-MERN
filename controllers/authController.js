0e41a4731e4b081dbcaa49315828e7c6d131657c
BaGorK <edmealemkassahun@gmail.com>
2024-04-18 14:33:24 +0300

    feat: hashing a password using bcryptjs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,21 @@
 import { StatusCodes } from 'http-status-codes';
+import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+
   const user = await User.create({ ...req.body, role });
 
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({
+    status: 'success',
+    msg: 'user created',
+  });
 };
 
 export const login = async (req, res) => {
