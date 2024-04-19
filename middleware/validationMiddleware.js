052ae261d07210c3544cc6184c04a3b99ae06983
BaGorK <edmealemkassahun@gmail.com>
2024-04-19 19:26:31 +0300

    feat: create a validation layer for update user

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -96,3 +96,20 @@ export const validateLoginInput = withValidationErrors([
     .isLength({ min: 8 })
     .withMessage('password should have at least 8 chars'),
 ]);
+
+export const validateUpdateUserInput = withValidationErrors([
+  body('name').notEmpty().withMessage('name is required'),
+  body('email')
+    .notEmpty()
+    .withMessage('email is required')
+    .isEmail()
+    .withMessage('invalid email format')
+    .custom(async (email, { req }) => {
+      const user = await User.findOne({ email });
+      if (user && user._id.toString() !== req.user.userId) {
+        throw new Error('email already exists');
+      }
+    }),
+  body('lastName').notEmpty().withMessage('last name is required'),
+  body('location').notEmpty().withMessage('location is required'),
+]);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,11 +4,12 @@ import {
   getCurrentUser,
   updateUser,
 } from '../controllers/userController.js';
+import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
 
 const Router = express.Router();
 
 Router.get('/current-user', getCurrentUser);
-Router.patch('/update-user', updateUser);
+Router.patch('/update-user', validateUpdateUserInput, updateUser);
 
 Router.get('/admin/app-stats', getApplicationStats);
 
