6fcf5c95b5e6ef0fee39125e8bbc83388c829c4e
BaGorK <edmealemkassahun@gmail.com>
2024-04-19 17:34:45 +0300

    feat: verify the token | get payload from

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -7,3 +7,9 @@ export const createJWT = (payload) => {
 
   return token;
 };
+
+export const verifyJWT = (token) => {
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  return decoded;
+};
