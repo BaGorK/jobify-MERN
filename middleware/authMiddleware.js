d6b48d52ff1d6f01fee47a72ac9f3db245f0a6e0
BaGorK <edmealemkassahun@gmail.com>
2024-04-19 19:46:58 +0300

    feat: add a validation layer on  getApplicationStats | authorizePermissions

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,7 @@
-import { UnauthenticatedError } from '../errors/customErrors.js';
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+} from '../errors/customErrors.js';
 import { verifyJWT } from '../utils/tokenUtils.js';
 
 export const authenticateUser = (req, res, next) => {
@@ -13,4 +16,14 @@ export const authenticateUser = (req, res, next) => {
   } catch (error) {
     throw new UnauthenticatedError('authentication invalid');
   }
+};
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    console.log('authorize', roles);
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError('Unauthorized to access this route');
+    }
+    next();
+  };
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,12 +5,16 @@ import {
   updateUser,
 } from '../controllers/userController.js';
 import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
+import { authorizePermissions } from '../middleware/authMiddleware.js';
 
 const Router = express.Router();
 
 Router.get('/current-user', getCurrentUser);
 Router.patch('/update-user', validateUpdateUserInput, updateUser);
 
-Router.get('/admin/app-stats', getApplicationStats);
+Router.get('/admin/app-stats', [
+  authorizePermissions('admin'),
+  getApplicationStats,
+]);
 
 export default Router;
