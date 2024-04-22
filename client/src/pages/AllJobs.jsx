9a17a8e58d2bb72885bbbbfd4281a9bbd111750d
BaGorK <edmealemkassahun@gmail.com>
2024-04-22 17:44:41 +0300

    feat: add a loader in allJobs.jsx

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,6 +1,24 @@
+import { toast } from 'react-toastify';
 import { JobsContainer, SearchContainer } from '../components';
+import customFetch from '../utils/customFetch';
+import { useLoaderData } from 'react-router-dom';
+
+export const loader = async ({ request }) => {
+  try {
+    const { data } = await customFetch.get('/jobs');
+    return {
+      data,
+    };
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 function AllJobs() {
+  const { data } = useLoaderData();
+  console.log(data);
+
   return (
     <>
       <SearchContainer />
