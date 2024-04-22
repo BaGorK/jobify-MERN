ad421bdd75714691c9a4285297b5175429d6b373
BaGorK <edmealemkassahun@gmail.com>
2024-04-22 18:00:06 +0300

    feat: render jobs | jobsContainer.jsx

---
+++ client/src/components/Job.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+
+function Job({ company }) {
+  return <h1>{company}</h1>;
+}
+
+export default Job;
--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,5 +1,28 @@
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useAllJobsContext } from '../pages/AllJobs';
+import Job from './Job';
+
 function JobsContainer() {
-  return <h1>Jobs container</h1>;
+  const {
+    data: { jobs },
+  } = useAllJobsContext();
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 }
 
 export default JobsContainer;
