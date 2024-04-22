7774f5cf3ddd83cb907f72ed83dfc3701cb0def6
BaGorK <edmealemkassahun@gmail.com>
2024-04-22 22:48:21 +0300

    feat: accessing the route params in the loader and the component | EditJob.jsx

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,12 +1,14 @@
 import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
 import { Form, useNavigation, redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import customFetch from '../utils/customFetch';
 
-export const loader = async () => {
+export const loader = async ({ params }) => {
+  console.log(params);
+
   return null;
 };
 export const action = async () => {
@@ -14,6 +16,9 @@ export const action = async () => {
 };
 
 function EditJob() {
+  const params = useParams();
+  console.log(params);
+
   return <div>Edit job</div>;
 }
 
