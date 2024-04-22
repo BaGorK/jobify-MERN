85cf2332d5e10ab622568b7de3c351d2d5ec30b1
BaGorK <edmealemkassahun@gmail.com>
2024-04-22 22:44:09 +0300

    feat: setting up a loader and an action | EditJob.jsx

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,3 +1,18 @@
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useLoaderData } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+export const loader = async () => {
+  return null;
+};
+export const action = async () => {
+  return null;
+};
+
 function EditJob() {
   return <div>Edit job</div>;
 }
