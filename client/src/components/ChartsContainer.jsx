21db29a73b8fed15858cca5517dd3c1883a78a35
BaGorK <edmealemkassahun@gmail.com>
2024-05-01 02:43:42 +0300

    update: chartsContainer

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
 function ChartsContainer({ data }) {
-  return <div>Charts container</div>;
+  const [barChart, setBarChart] = useState(true);
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Areat Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 }
 
 export default ChartsContainer;
