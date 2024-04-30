29da28cb316f4715ffbfd522ef0c40834d7421cc
BaGorK <edmealemkassahun@gmail.com>
2024-05-01 01:34:56 +0300

    feat: use mongodb aggregate pipeline | group job with jobStatus and  count

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -71,10 +71,24 @@ export const deleteJob = async (req, res) => {
 };
 
 export const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: '$jobStatus', count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, cur) => {
+    const { _id: title, count } = cur;
+    acc[title] = count;
+
+    return acc;
+  }, {});
+
+  console.log(stats);
+
   const defaultStats = {
-    pending: 22,
-    interview: 11,
-    declined: 4,
+    pending: stats?.pending || 0,
+    interview: stats?.interview || 0,
+    declined: stats?.declined || 0,
   };
 
   let monthlyApplications = [
