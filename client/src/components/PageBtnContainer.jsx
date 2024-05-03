891d090bca713831f21c3f799d8a6b6565758922
BaGorK <edmealemkassahun@gmail.com>
2024-05-03 10:51:06 +0300

    feat: render prev and next buttons

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -5,11 +5,31 @@ import { useAllJobsContext } from '../pages/AllJobs';
 
 function PageBtnContainer() {
   const {
-    data: { totalJobs, numOfPages, currentPage },
+    data: { numOfPages, currentPage },
   } = useAllJobsContext();
-  console.log(totalJobs, numOfPages, currentPage);
 
-  return <div>PageBtnContainer</div>;
+  const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
+
+  return (
+    <Wrapper>
+      <button className='btn prev-btn'>
+        <HiChevronDoubleLeft /> prev
+      </button>
+      <div className='btn-container'>
+        {pages.map((pageNumber) => (
+          <button
+            className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
+            key={pageNumber}
+          >
+            {pageNumber}
+          </button>
+        ))}
+      </div>
+      <button className='btn next-btn'>
+        next <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
 }
 
 export default PageBtnContainer;
