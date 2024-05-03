fd962b70b53caaf376d8293dc9fb6fb5a12522f2
BaGorK <edmealemkassahun@gmail.com>
2024-05-03 11:46:59 +0300

    feat: page logic - complex approach

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -3,13 +3,10 @@ import Wrapper from '../assets/wrappers/PageBtnContainer';
 import { useLocation, Link, useNavigate } from 'react-router-dom';
 import { useAllJobsContext } from '../pages/AllJobs';
 
-function PageBtnContainer() {
+const PageBtnContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
-
-  const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
-
   const { search, pathname } = useLocation();
   const navigate = useNavigate();
 
@@ -19,29 +16,86 @@ function PageBtnContainer() {
     navigate(`${pathname}?${searchParams.toString()}`);
   };
 
+  const addPageButton = ({ pageNumber, activeClass }) => {
+    return (
+      <button
+        className={`btn page-btn ${activeClass && 'active'}`}
+        key={pageNumber}
+        onClick={() => handlePageChange(pageNumber)}
+      >
+        {pageNumber}
+      </button>
+    );
+  };
+
+  const renderPageButtons = () => {
+    const pageButtons = [];
+
+    // Add the first page button
+    pageButtons.push(
+      addPageButton({ pageNumber: 1, activeClass: currentPage === 1 })
+    );
+    // Add the dots before the current page if there are more than 3 pages
+    if (currentPage > 3) {
+      pageButtons.push(
+        <span className='page-btn dots' key='dots-1'>
+          ....
+        </span>
+      );
+    }
+    // one before current page
+    if (currentPage !== 1 && currentPage !== 2) {
+      pageButtons.push(
+        addPageButton({ pageNumber: currentPage - 1, activeClass: false })
+      );
+    }
+
+    // Add the current page button
+    if (currentPage !== 1 && currentPage !== numOfPages) {
+      pageButtons.push(
+        addPageButton({ pageNumber: currentPage, activeClass: true })
+      );
+    }
+
+    // one after current page
+    if (currentPage !== numOfPages && currentPage !== numOfPages - 1) {
+      pageButtons.push(
+        addPageButton({ pageNumber: currentPage + 1, activeClass: false })
+      );
+    }
+    if (currentPage < numOfPages - 2) {
+      pageButtons.push(
+        <span className=' page-btn dots' key='dots+1'>
+          ....
+        </span>
+      );
+    }
+
+    // Add the last page button
+    pageButtons.push(
+      addPageButton({
+        pageNumber: numOfPages,
+        activeClass: currentPage === numOfPages,
+      })
+    );
+
+    return pageButtons;
+  };
+
   return (
     <Wrapper>
       <button
-        className='btn prev-btn'
+        className='prev-btn'
         onClick={() => {
           let prevPage = currentPage - 1;
           if (prevPage < 1) prevPage = numOfPages;
           handlePageChange(prevPage);
         }}
       >
-        <HiChevronDoubleLeft /> prev
+        <HiChevronDoubleLeft />
+        prev
       </button>
-      <div className='btn-container'>
-        {pages.map((pageNumber) => (
-          <button
-            className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
-            key={pageNumber}
-            onClick={() => handlePageChange(pageNumber)}
-          >
-            {pageNumber}
-          </button>
-        ))}
-      </div>
+      <div className='btn-container'>{renderPageButtons()}</div>
       <button
         className='btn next-btn'
         onClick={() => {
@@ -50,10 +104,10 @@ function PageBtnContainer() {
           handlePageChange(nextPage);
         }}
       >
-        next <HiChevronDoubleRight />
+        next
+        <HiChevronDoubleRight />
       </button>
     </Wrapper>
   );
-}
-
+};
 export default PageBtnContainer;
