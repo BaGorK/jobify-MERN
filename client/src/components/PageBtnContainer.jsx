672cefbb8d18876e67fc543ca2a582b771d3bb98
BaGorK <edmealemkassahun@gmail.com>
2024-05-03 11:30:59 +0300

    feat: pagination logic

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -10,9 +10,25 @@ function PageBtnContainer() {
 
   const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
 
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
   return (
     <Wrapper>
-      <button className='btn prev-btn'>
+      <button
+        className='btn prev-btn'
+        onClick={() => {
+          let prevPage = currentPage - 1;
+          if (prevPage < 1) prevPage = numOfPages;
+          handlePageChange(prevPage);
+        }}
+      >
         <HiChevronDoubleLeft /> prev
       </button>
       <div className='btn-container'>
@@ -20,12 +36,20 @@ function PageBtnContainer() {
           <button
             className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
             key={pageNumber}
+            onClick={() => handlePageChange(pageNumber)}
           >
             {pageNumber}
           </button>
         ))}
       </div>
-      <button className='btn next-btn'>
+      <button
+        className='btn next-btn'
+        onClick={() => {
+          let nextPage = currentPage + 1;
+          if (nextPage > numOfPages) nextPage = 1;
+          handlePageChange(nextPage);
+        }}
+      >
         next <HiChevronDoubleRight />
       </button>
     </Wrapper>
--- controllers/jobController.js
+++ controllers/jobController.js
@@ -45,7 +45,7 @@ export const getAllJobs = async (req, res) => {
 
   // pagination
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 15;
+  const limit = Number(req.query.limit) || 10;
 
   const skip = (page - 1) * limit;
 
