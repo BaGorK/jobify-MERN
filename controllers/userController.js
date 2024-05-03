e72076843930da5bfa5ba8eb566def69849c9a9a
BaGorK <edmealemkassahun@gmail.com>
2024-05-03 21:58:59 +0300

    fix: upload image using diskStorage does not work on render platform

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
 
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import Job from '../models/jobModel.js';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findById(req.user.userId);
@@ -21,8 +21,10 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+
+    const response = await cloudinary.v2.uploader.upload(file);
+
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
